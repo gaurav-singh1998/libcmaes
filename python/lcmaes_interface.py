120a412a84315ef7ff8a4a194b1e4c69ae9236d3
nikohansen <nikohansen@users.noreply.github.com>
2015-02-14 18:47:50 +0100

    improved outfile logic
    to_params sets the current outfile, which becomes by default the one which is plotted

--- python/lcmaes_interface.py
+++ python/lcmaes_interface.py
@@ -13,19 +13,22 @@ but that might change in future to expose more parameters.
 
 import lcmaes as cmaes
 import cma_multiplt as cmaplt
-outfile = 'lcmaes.dat'
+outfile_current = 'lcmaes.dat'
 
-def to_params(x0, sigma0, strategy="acmaes", outfile=outfile):
+def to_params(x0, sigma0, strategy="acmaes", outfile=None):
     """return parameter object instance for ``lcmaes.pcmaes``"""
+    if outfile:
+        global outfile_current
+        outfile_current = outfile
     p = cmaes.make_simple_parameters(x0, sigma0)
     p.set_str_algo("acmaes")
-    p.set_fplot(outfile)
+    p.set_fplot(outfile_current)
     return p
 
 def to_fitfunc(f):
     """return function from callable `f`, where `f` accepts a list of numbers as input."""
     return cmaes.fitfunc_pbf.from_callable(lambda x, n: f(x))
     
-def plot(outfile=outfile):
-    cmaplt.plot(outfile)
+def plot(outfile=None):
+    cmaplt.plot(outfile if outfile else outfile_current)
     
