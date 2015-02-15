bdc91b8bb9677449fe0dd3e1420682d9cd795e04
nikohansen <nikohansen@users.noreply.github.com>
2015-02-15 01:23:35 +0100

    "restore" from origin

--- python/simple.py
+++ python/simple.py
@@ -2,21 +2,21 @@ import lcmaes
 import cma_multiplt as lcmaplt
 
 # input parameters for a 10-D problem
-x = [2.0] * 10
+x = [10]*10
 sigma = 0.1
-outfile = 'simple.dat'
-
-p = lcmaes.make_simple_parameters(x, sigma)
+p = lcmaes.make_simple_parameters(x,sigma)
 p.set_str_algo("acmaes")
-p.set_fplot(outfile)
+p.set_fplot('simple.dat')
 
 # objective function.
-def nfitfunc(x, n):
-    assert len(x) == n  # should not be necessary
-    return sum([xi**2 for xi in x])
-    
+def nfitfunc(x,n):
+    val = 0.0
+    for i in range(0,n):
+        val += x[i]*x[i]
+    return val
+
 # pass the function and parameters to cmaes, run optimization and collect solution object.
-cmasols = lcmaes.pcmaes(lcmaes.fitfunc_pbf.from_callable(nfitfunc), p)
+cmasols = lcmaes.pcmaes(lcmaes.fitfunc_pbf.from_callable(nfitfunc),p)
 
 # visualize results
-lcmaplt.plot(outfile)
+lcmaplt.plot('simple.dat')
